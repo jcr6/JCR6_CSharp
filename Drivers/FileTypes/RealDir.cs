fb9cba4b3977e0abc842f200ae3badfd901082a8
Tricky1975 <facebook@tbbs.nl>
2018-08-26 22:58:58 +0200

    Lic

--- Drivers/FileTypes/RealDir.cs
+++ Drivers/FileTypes/RealDir.cs
@@ -1,3 +1,13 @@
+// Lic:
+//         RealDir.cs
+// 	(c) 2018 Jeroen Petrus Broks.
+// 	
+// 	This Source Code Form is subject to the terms of the 
+// 	Mozilla Public License, v. 2.0. If a copy of the MPL was not 
+// 	distributed with this file, You can obtain one at 
+// 	http://mozilla.org/MPL/2.0/.
+//         Version: 18.08.26
+// EndLic
 using System.IO;
 using System.Collections.Generic;
 
